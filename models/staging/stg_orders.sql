630102df6b5648673009b140346b1e94ffb40229
Nazmi Aydın <aydinn@mef.edu.tr>
2023-08-20 09:55:12 +0000

    developing stage orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+SELECT 
+--from raw orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+ordersellingprice - ordercostprice as orderprofit,
+ordersellingprice,
+ordercostprice,
+--from raw customer
+c.customername,
+c.segment,
+c.country,
+--from raw product
+p.category,
+p.productname,
+p.subcategory
+FROM {{ ref('raw_orders') }} as o 
+LEFT JOIN {{ ref('raw_customer') }} as c
+ON o.customerid = c.customerid
+LEFT JOIN {{ ref('raw_product') }} as p
+ON o.productid = p.productid

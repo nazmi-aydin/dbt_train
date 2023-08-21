a481ca8248590f87fce4b97815459dc2ecf39435
Nazmi Aydın <aydinn@mef.edu.tr>
2023-08-21 19:03:17 +0000

    developed report_profit_by_customer and report_profit_by_product

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -8,10 +8,12 @@ ordersellingprice - ordercostprice as orderprofit,
 ordersellingprice,
 ordercostprice,
 --from raw customer
+c.customerid,
 c.customername,
 c.segment,
 c.country,
 --from raw product
+p.productid,
 p.category,
 p.productname,
 p.subcategory
